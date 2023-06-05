964ec22922992186e9069b336059b9c2bfa2ffb1
mendsalbert <mendsalbert@gmail.com>
2023-06-05 23:07:25 +0000

    sploriteai

--- contracts/SubscriptionModel.sol
+++ contracts/SubscriptionModel.sol
@@ -1,44 +1,83 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract SubscriptionModel {
-    address payable public contractOwner;
-    uint256 public subscriptionFee = 0.0053 ether;
-    mapping(address => bool) public subscriptions;
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AlarmClockInterface.sol";
 
-    event SubscriptionStatus(address user, bool isSubscribed);
-    event PaymentReceived(address user, uint256 amount);
+contract SubscriptionContract {
+    address public owner;
+    mapping(address => bool) public subscribers;
+    mapping(address => uint256) public subscriptionExpiry;
+    uint256 public subscriptionFee;
+    AggregatorV3Interface public priceFeed;
+    AlarmClockInterface public alarmClock;
 
-    constructor() {
-        contractOwner = payable(msg.sender);
-    }
-    
-    function subscribe() public payable {
-        require(!subscriptions[msg.sender], "Already subscribed");
-        require(msg.value == subscriptionFee, "Invalid payment amount");
+    event SubscriptionRenewed(address indexed subscriber, uint256 timestamp);
+    event SubscriptionCancelled(address indexed subscriber, uint256 timestamp);
+    event SubscriptionExpired(address indexed subscriber, uint256 timestamp);
 
-        subscriptions[msg.sender] = true;
-        emit SubscriptionStatus(msg.sender, true);
-        emit PaymentReceived(msg.sender, msg.value);
-    }
-
-    function unsubscribe() public {
-        require(subscriptions[msg.sender], "Not subscribed");
-
-        subscriptions[msg.sender] = false;
-        emit SubscriptionStatus(msg.sender, false);
+    constructor(address _priceFeed, address _alarmClock) {
+        owner = msg.sender;
+        subscriptionFee = 9990000000000000000000; // 9.99 USD (converted to wei)
+        priceFeed = AggregatorV3Interface(_priceFeed);
+        alarmClock = AlarmClockInterface(_alarmClock);
     }
 
-    function isSubscribed() public view returns (bool) {
-        return subscriptions[msg.sender];
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the contract owner can call this function");
+        _;
     }
 
-    function withdrawFunds() public {
-        require(msg.sender == contractOwner, "Only contract owner can withdraw funds");
+    function subscribe() external {
+        require(!subscribers[msg.sender], "Already subscribed");
 
-        uint256 contractBalance = address(this).balance;
-        require(contractBalance > 0, "No funds available");
+        require(
+            getCurrentEthPrice() >= subscriptionFee,
+            "Insufficient funds to subscribe"
+        );
 
-        contractOwner.transfer(contractBalance);
+        subscribers[msg.sender] = true;
+        subscriptionExpiry[msg.sender] = alarmClock.getCurrentAlarmTime() + 30 days; // Set subscription expiry after 30 days
+
+        emit SubscriptionRenewed(msg.sender, block.timestamp);
+    }
+
+    function unsubscribe() external {
+        require(subscribers[msg.sender], "Not subscribed");
+
+        subscribers[msg.sender] = false;
+        subscriptionExpiry[msg.sender] = 0;
+
+        emit SubscriptionCancelled(msg.sender, block.timestamp);
+    }
+
+    function isSubscribed(address subscriber) external view returns (bool) {
+        return subscribers[subscriber] && (subscriptionExpiry[subscriber] > block.timestamp);
+    }
+
+    function setSubscriptionFee(uint256 newFee) external onlyOwner {
+        subscriptionFee = newFee;
+    }
+
+    function getCurrentEthPrice() internal view returns (uint256) {
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        require(price > 0, "Invalid price feed");
+        uint256 ethPrice = uint256(price) * 100; // ETH price with 2 decimal places precision
+        return (ethPrice * 10**10) / 10**8; // Convert ETH price to wei
+    }
+
+    function alarmTriggered(bytes32 _jobId) external {
+        require(msg.sender == address(alarmClock), "Invalid caller");
+        require(_jobId == alarmClock.getJobId(), "Invalid job ID");
+
+        // Expire subscriptions that have reached the expiry time
+        for (uint256 i = 0; i < subscribers.length; i++) {
+            address subscriber = subscribers[i];
+            if (subscriptionExpiry[subscriber] <= block.timestamp) {
+                subscribers[subscriber] = false;
+                subscriptionExpiry[subscriber] = 0;
+                emit SubscriptionExpired(subscriber, block.timestamp);
+            }
+        }
     }
 }
